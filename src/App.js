f3c8df8818531ce59cf6f995f541412d0511d0b8
Dani F. <114934334+Yuf0ria@users.noreply.github.com>
2024-09-11 14:51:58 +0800

    changed p font size and color background

--- src/App.js
+++ src/App.js
@@ -7,19 +7,22 @@ function App() {
         <header className="App-header">
         <h3>Jane Doe</h3>
         <p>Age:30
-        <br/> Location: New York</p>
+        <br/><br/> 
+        Location: New York</p>
         </header>
 
         <header className="App-header">
         <h3>Jane Smith</h3>
         <p>Age:25
-        <br/> Location: San Francisco</p>
+        <br/><br/>
+        Location: San Francisco</p>
         </header>
 
         <header className="App-header">
         <h3>Ace Reyes</h3>
         <p>Age:27
-        <br/> Location: Valenzuela</p>
+        <br/><br/>
+        Location: Valenzuela</p>
         </header>
       </div>
     </body>

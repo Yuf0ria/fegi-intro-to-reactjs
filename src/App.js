3a62a640d64d8af5dc765ffa9db970ef286b824c
Dani F. <114934334+Yuf0ria@users.noreply.github.com>
2024-09-11 14:46:48 +0800

    Updated app.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <body>
       <div className="App">
-    <header className="App-header">
-    <h3>Jane Doe</h3>
-    <p>Age:30
-    <br/> Location: New York</p>
-    </header>
-    <header className="App-header">
-    <h3>Jane Smith</h3>
-    <p>Age:25
-    <br/> Location: San Francisco</p>
-    </header>
-    <header className="App-header">
-    <h3>Ace Reyes</h3>
-    <p>Age:27
-    <br/> Location: Valenzuela</p>
-    </header>
-  </div>
-);</body>
+        <header className="App-header">
+        <h3>Jane Doe</h3>
+        <p>Age:30
+        <br/> Location: New York</p>
+        </header>
+
+        <header className="App-header">
+        <h3>Jane Smith</h3>
+        <p>Age:25
+        <br/> Location: San Francisco</p>
+        </header>
+
+        <header className="App-header">
+        <h3>Ace Reyes</h3>
+        <p>Age:27
+        <br/> Location: Valenzuela</p>
+        </header>
+      </div>
+    </body>
   );
 }
 
